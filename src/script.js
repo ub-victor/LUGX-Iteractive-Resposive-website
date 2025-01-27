73fc45d562b7399bfd56beb86473bdb5d04f7982
ub-victor <victoireushindi371@gmail.com>
2025-01-27 15:12:25 +0200

    test generator

--- src/script.js
+++ src/script.js
@@ -1,14 +1,28 @@
 window.onload = function() {
-    function* gen(){
-        var x = yield  'pear' ;
-        var y = yield 'banana';
-        var z = yield 'apple';
-        return x + y + z;
-    }
-    var myGen = gen();
-    console.log(myGen.next());
-    console.log(myGen.next(10));
-    console.log(myGen.next(5));
-    console.log(myGen.next(3)); 
-    
-}
+     genWrapper(function* generator(){
+        var tweets = yield $.getJSON("data/tweets.json");
+        console.log(tweets);
+        var friends = yield $.getJSON('data/friends.json');
+        console.log(friends);
+        var ytVids = yield $.getJSON('data/youtube.json');
+        console.log(ytVids);
+    });
+
+
+function genWrapper(generator){
+    // set up generator
+    var myGen = generator();
+
+    // create function to handle the yielded value  
+    function handle(yielded){
+        if(!yielded.done){
+            yielded.value.then(function(data){
+                return handle(myGen.next(data));
+            });
+        }
+    }//end handle
+    // return function , passing in myGen.next
+    return handle(myGen.next()); 
+}// end genWrapper
+
+};
